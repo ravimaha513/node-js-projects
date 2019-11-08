0515e3b83233dba07a3de2b4c42c552d1730474e
Ravi Teja <raviteja@Ravis-Air.lan1>
2019-11-07 22:00:33 -0500

    Adding mongoose

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var http = require('http');
 
-http.createServer(function(req, res) {
-	res.writeHead(200, {
-		'Content-Type' : 'text/plain'
-	});
-	res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
+var express = require('express');
 
-console.log('Server running at http://127.0.0.1:3000/');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+var cats = require('./cat_routes.js')(app);
+
+
+var server = app.listen(3000, function(){
+	console.log('Server running at http://127.0.0.1:3000/');
+});
+
